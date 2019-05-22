0f603cbeaa055c95902a410f1c4664b47b686621
AKASHgkr <49703165+AKASHgkr@users.noreply.github.com>
2019-05-22 13:07:07 +0530

    Update Code_Selection_Sort.cpp

--- Code_Selection_Sort.cpp
+++ Code_Selection_Sort.cpp
@@ -1,48 +1,35 @@
-/*C++ code on selection sort*/
-#include <iostream.h>
- 
-using namespace std;
- 
-// Sort arr[] of size n using Selection Sort. 
-void SelectionSort (int arr[], int n)
+/* C++ code on selection sort */
+#include<iostream.h>
+#include<conio.h>
+void main()
 {
-	int i, j;
-	for (i = 0; i < n; ++i)
-	{
-		for (j = i+1; j < n; ++j)
-		{
-			// Comparing consecutive data and switching values if value at i > j.
-			if (arr[i] > arr[j])
-			{
-				arr[i] = arr[i]+arr[j];
-				arr[j] = arr[i]-arr[j];
-				arr[i] = arr[i]-arr[j];
-			}	
-		}
-		// Value at i will be minimum of all the values above this index.
-	}	
-} }	
-int main()
+clrscr();
+int n,i,j;
+int arr[1000];
+cout<<"Input the Number of elements";
+cin>>n;
+for(i=0;i<n;i++)
 {
-	int n, i;
-	cout<<"\nEnter the number of data element to be sorted: ";
-	cin>>n;
- 
-	int arr[n];
-	for(i = 0; i < n; i++)
-	{
-		cout<<"Enter element "<<i+1<<": ";
-		cin>>arr[i];
-	}
- 
-	SelectionSort(arr, n);
- 
-	// Display the sorted data
-  
-  // Display the sorted data.
-	cout<<"\nSorted Data ";
-	for (i = 0; i < n; i++)
-		cout<<"->"<<arr[i];
- 
-	return 0;
+cout<<"Enter element"<<i+1<":";
+cin>>arr[i];
+}
+for(i=0;i<n;i++)
+{
+   for(j=i+1;j<n;j++)
+   {
+      if(arr[i]>arr[j]
+      {
+      arr[i]=arr[i]+arr[j];
+      arr[j]=arr[i]-arr[j];
+      arr[i]=arr[i]-arr[j];
+      }
+    }  
+ }
+ cout("Sorted array");
+ for(i=0;i<n;i++)
+ {
+ cout<<arr[i]<<"\n";
+ }
+ getch();
+ return 0;
 } 
